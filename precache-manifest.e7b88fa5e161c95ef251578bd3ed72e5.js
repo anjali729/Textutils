b8efafd5c8a723f8d77f08fa11e3b3c8961b3a79
Anjali Porwal <anjaliporwal761@gmail.com>
2024-12-23 23:11:00 +0530

    Updates

---
+++ precache-manifest.e7b88fa5e161c95ef251578bd3ed72e5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "349e0a1f47432da58816ce0b9b15d2f7",
+    "url": "/Textutils/index.html"
+  },
+  {
+    "revision": "7bb3bebe7672d74b07ef",
+    "url": "/Textutils/static/css/main.5755b819.chunk.css"
+  },
+  {
+    "revision": "1582e325d1bcf091e744",
+    "url": "/Textutils/static/js/2.ca150098.chunk.js"
+  },
+  {
+    "revision": "2e7b5277f70c872aecfe",
+    "url": "/Textutils/static/js/3.940f5f6f.chunk.js"
+  },
+  {
+    "revision": "7bb3bebe7672d74b07ef",
+    "url": "/Textutils/static/js/main.54dcf8ef.chunk.js"
+  },
+  {
+    "revision": "f83609ed1ac253a67ec0",
+    "url": "/Textutils/static/js/runtime~main.894db38b.js"
+  }
+]);
